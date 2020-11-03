b67cd462fdbb1c01f534e1938b771d338be6cfff
Electronica y ciencia <electronicayciencia@gmail.com>
2020-11-03 11:54:04 +0100

    RTOS led blinking

---
+++ led_on_off-rtos/README.md
@@ -0,0 +1,9 @@
+# LED On Off using RTOS
+
+Non OS SDK is discontinued:
+
+> Support Policy for ESP8266 NonOS (Starting from December 2019)
+>
+> - We will not add any new features to the ESP8266 NonOS SDK.
+> - We will only fix critical bugs in the ESP8266 NonOS SDK.
+> - It is suggested that the ESP8266_RTOS_SDK, instead of ESP8266 NonOS > SDK, be used for your projects.
---
+++ led_on_off-rtos/src/main.c
@@ -0,0 +1,20 @@
+#include "esp_common.h"
+#include "freertos/task.h"
+#include "gpio.h"
+
+
+void task_blink(void* ignore) {
+    while(true) {
+		GPIO_OUTPUT_SET(2, 0);
+        vTaskDelay(100/portTICK_RATE_MS);
+		GPIO_OUTPUT_SET(2, 1);
+        vTaskDelay(100/portTICK_RATE_MS);
+    }
+    vTaskDelete(NULL);
+}
+
+void user_init(void) {
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2);
+    xTaskCreate(&task_blink, "startup", 2048, NULL, 1, NULL);
+}
+
