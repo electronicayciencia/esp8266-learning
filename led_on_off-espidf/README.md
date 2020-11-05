ec857e9f31c510f4f52ac83980e19ac3f4326bc2
Electronica y ciencia <electronicayciencia@gmail.com>
2020-11-05 16:08:49 +0100

    First blink working with ESP-IDF.

--- led_on_off-espidf/README.md
+++ led_on_off-espidf/README.md
@@ -1,3 +1,12 @@
 # Led On Off
 
-Blink a LED using ESP-IDF style.
+Blink a LED using ESP-IDF style (v3.3 stable)
+
+Edited .vscode/c_cpp_properties.json to add include paths.
+
+Compile under ESP tools environment (Windows or Linux)
+
+- make menuconfig (select serial port and flash size)
+- make
+- make flash
+- make monitor (or make simple_monitor)
--- led_on_off-espidf/main/main.c
+++ led_on_off-espidf/main/main.c
@@ -1,38 +1,23 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_spi_flash.h"
+#include <stdio.h> // printf
+#include "freertos/FreeRTOS.h" // portTICK_PERIOD_MS
+#include "freertos/task.h" // vTaskDelay
+#include <driver/gpio.h> // gpio_set_level & others
 
 
 void app_main()
 {
-    printf("Hello world!\n");
+	gpio_config_t io_conf;
+	io_conf.pin_bit_mask = GPIO_Pin_2;
+	io_conf.mode = GPIO_MODE_OUTPUT;
+	gpio_config(&io_conf);
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP8266 chip with %d CPU cores, WiFi, ",
-            chip_info.cores);
+	printf("Hello.\n");
 
-    printf("silicon revision %d, ", chip_info.revision);
+	while(true) {
+		gpio_set_level(GPIO_NUM_2, 1);
+		vTaskDelay(500/portTICK_PERIOD_MS);
+		gpio_set_level(GPIO_NUM_2, 0);
+		vTaskDelay(500/portTICK_PERIOD_MS);
+	}
 
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
 }
