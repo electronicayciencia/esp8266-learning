fb86e84f5bf2e219f2fc1226beb55f2bebd3c7e3
Electronica y ciencia <electronicayciencia@gmail.com>
2020-11-07 14:20:20 +0100

    Minor changes

--- sleep-flash/main/main.c
+++ sleep-flash/main/main.c
@@ -9,14 +9,28 @@
 #define LED_ON   0
 #define LED_OFF  1
 
+#define PIN_LED_1 GPIO_NUM_0
+#define PIN_LED_2 GPIO_NUM_2
 
-void task_flash_led() {
+#define PERIOD_1_MS 1000
+#define PERIOD_2_MS 150
+
+
+void task_flash_led_1() {
 	while(true) {
-		gpio_set_level(GPIO_NUM_2, LED_ON);
-		vTaskDelay(50/portTICK_PERIOD_MS);
-		gpio_set_level(GPIO_NUM_2, LED_OFF);
+		gpio_set_level(PIN_LED_1, LED_ON);
+		vTaskDelay(PERIOD_1_MS/2/portTICK_PERIOD_MS);
+		gpio_set_level(PIN_LED_1, LED_OFF);
+		vTaskDelay(PERIOD_1_MS/2/portTICK_PERIOD_MS);
+	}
+}
 
-		vTaskDelay(1500/portTICK_PERIOD_MS);
+void task_flash_led_2() {
+	while(true) {
+		gpio_set_level(PIN_LED_2, LED_ON);
+		vTaskDelay(PERIOD_2_MS/2/portTICK_PERIOD_MS);
+		gpio_set_level(PIN_LED_2, LED_OFF);
+		vTaskDelay(PERIOD_2_MS/2/portTICK_PERIOD_MS);
 	}
 }
 
@@ -24,14 +38,15 @@ void task_flash_led() {
 void app_main()
 {
 	gpio_config_t io_conf;
-	io_conf.pin_bit_mask = GPIO_Pin_2;
+	io_conf.pin_bit_mask = (1 << PIN_LED_1) | (1 << PIN_LED_2);
 	io_conf.mode = GPIO_MODE_OUTPUT;
 	io_conf.intr_type = GPIO_INTR_DISABLE;
 	gpio_config(&io_conf);
 
 	gpio_set_level(GPIO_NUM_2, LED_OFF);
 
-	xTaskCreate(task_flash_led, "flash_led", 1024, NULL, 5, NULL);
+	xTaskCreate(task_flash_led_1, "flash_led_1", 1024, NULL, 5, NULL);
+	xTaskCreate(task_flash_led_2, "flash_led_2", 1024, NULL, 5, NULL);
 	ESP_LOGI("main","End. Go to bed.");
 	
 }
