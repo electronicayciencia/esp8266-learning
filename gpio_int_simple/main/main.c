7067bad3154e108b8045a28555b0b8e123c5911e
Electronica y Ciencia <electronicayciencia@gmail.com>
2021-01-20 17:30:38 +0100

    Detect missing interruption.

--- gpio_int_simple/main/main.c
+++ gpio_int_simple/main/main.c
@@ -14,6 +14,7 @@
 
 #include "esp_log.h"
 #include "esp_system.h"
+#include "esp_timer.h" // high resolution timer
 
 static const char *TAG = "main";
 
@@ -28,37 +29,56 @@ static const char *TAG = "main";
  * Connect a button between GPIO2 and ground
  */
 
-#define GPIO_INPUT          2
-#define GPIO_INPUT_MASK    (1ULL<<GPIO_INPUT) 
+#define PIN_INPUT          GPIO_NUM_2
+
 
 static xQueueHandle gpio_evt_queue = NULL;
 
-static void gpio_isr_handler(void *arg)
-{
-    uint32_t gpio_num = (uint32_t) arg;
-    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+static void isr_pin_input_handler(void *arg) {
+    uint32_t io_num = PIN_INPUT;
+    xQueueSendFromISR(gpio_evt_queue, &io_num, NULL);
 }
 
-static void gpio_task_example(void *arg)
-{
+static void gpio_task_example(void *arg) {
     uint32_t io_num;
+    static int64_t last_interrupt = 0;
+    static int last_status = 0;
 
     for (;;) {
         if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
-            ESP_LOGI(TAG, "GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+            printf("GPIO[%d] intr, val: %d, elapsed: %d\n", 
+                io_num, 
+                gpio_get_level(io_num), 
+                (int32_t) (esp_timer_get_time() - last_interrupt));
+
+            if (gpio_get_level(io_num) == last_status) {
+                ESP_LOGW(TAG, "Missed interruption.");
+            }
+
+            last_status = gpio_get_level(io_num);
+            last_interrupt = esp_timer_get_time();
         }
     }
 }
 
 void app_main(void)
 {
+
+
+    /* Equivalent code, useful to configure multiple pins at once
     gpio_config_t io_conf;
 
     io_conf.intr_type = GPIO_INTR_ANYEDGE;
-    io_conf.pin_bit_mask = GPIO_INPUT_MASK;
+    io_conf.pin_bit_mask = (1ULL<<PIN_INPUT);
     io_conf.mode = GPIO_MODE_INPUT;
     io_conf.pull_up_en = 1;
     gpio_config(&io_conf);
+    */
+
+    gpio_set_intr_type(PIN_INPUT, GPIO_INTR_ANYEDGE);
+    gpio_set_direction(PIN_INPUT, GPIO_MODE_INPUT);
+    gpio_pullup_en(PIN_INPUT);
+
 
     //create a queue to handle gpio event from isr
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
@@ -68,11 +88,11 @@ void app_main(void)
     //install gpio isr service
     gpio_install_isr_service(0);
     //hook isr handler for specific gpio pin
-    gpio_isr_handler_add(GPIO_INPUT, gpio_isr_handler, (void *) GPIO_INPUT);
+    gpio_isr_handler_add(PIN_INPUT, isr_pin_input_handler, NULL);
 
     while (1) {
+        ESP_LOGI(TAG, "Running...");
         vTaskDelay(10000 / portTICK_RATE_MS);
-        printf("Waiting, push a button...\n");
     }
 }
 
